42476a00ac83b3611f9e5fbbab7bdc3ea0da657a
cameron-armand <camerondarmand@yahoo.com>
2020-07-29 20:06:11 -0500

    Update lazy.js

--- js/lazy.js
+++ js/lazy.js
@@ -1,20 +1,8 @@
-﻿const images = document.querySelectorAll("[data-src]")
-
-function preloadImage(img) {
-    const src = img.getAttribute("data-src")
-    if (!src) {
-        return;
-    }
-    else {
-        img.src = src;
-        img.className = "fade-in p-1";
-    }
-}
-
+const images = document.querySelectorAll("[data-src]")
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px -200px 0px"
+    rootMargin: "0px 0px -50px 0px"
 };
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
@@ -22,7 +10,8 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
         if (!entry.isIntersecting) {
             return;
         } else {
-            preloadImage(entry.target);
+            queue.loadFile({ id: entry.target.getAttribute("id"), src: entry.target.getAttribute("data-src") }); 
+            //preloadImage(entry.target);
             imgObserver.unobserve(entry.target);
         }
     })
@@ -32,3 +21,21 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 images.forEach(image => {
     imgObserver.observe(image);
 });
+
+var queue = new createjs.LoadQueue(false);
+
+queue.on("fileload", handleFileLoad);
+
+function handleFileLoad(event) {
+    //const src = event.item; // A reference to the item that was passed in to the LoadQueue
+    var item = event.item; // A reference to the item that was passed in to the LoadQueue
+    const loadedImage = event.result;
+    loadedImage.className = "fade-in p-1";
+    item2 = document.getElementById(item.id);
+    item2.appendChild(loadedImage);
+}
+
+var allImages = [];
+
+//img.className = "fade-in p-1";
+
